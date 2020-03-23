51eb4e8deb2a828c3c0c8efe8ef0919995a16388
pansy <526915701@qq.com>
2020-03-23 11:02:29 +0800

    init commit

---
+++ src/main/java/com/spike/myshop/service/redis/api/RedisService.java
@@ -0,0 +1,8 @@
+package com.spike.myshop.service.redis.api;
+
+public interface RedisService {
+    void set(String key, Object value);
+    void set(String key, Object value, int seconds);
+    void del(String key);
+    Object get(String key);
+}
